878b2aec085f7a6e4bb1f759029c4df09d89f8b3
co2mott <matthijswietsma@gmail.com>
2025-09-17 18:31:09 +0200

    UC06: Login functionaliteit geïmplementeerd
    - Client class aangepast naar Properties
    - ClientRepository uitgebreid om juiste Client op te halen i.p.v. vaste waarde
    - AuthService verder uitgewerkt met password controle
    - LoginView toegevoegd aan Grocery.App project (Views folder) en buildbaar gemaakt
    - Registraties van AuthService en LoginView geactiveerd in MauiProgram
    - LoginViewModel beschikbaar gemaakt in App.xaml.cs
    - MainPage omgezet naar LoginView (AppShell uitgeschakeld)
    - Route naar LoginView in AppShell.xaml.cs geactiveerd

--- Grocery.App/Views/LoginView.xaml.cs
+++ Grocery.App/Views/LoginView.xaml.cs
@@ -1,12 +1,12 @@
-using Grocery.App.ViewModels;
+﻿using Grocery.App.ViewModels;
 
 namespace Grocery.App.Views;
 
 public partial class LoginView : ContentPage
 {
-	public LoginView(LoginViewModel viewModel)
-	{
-		InitializeComponent();
-		BindingContext = viewModel;
-	}
+    public LoginView(LoginViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+    }
 }
